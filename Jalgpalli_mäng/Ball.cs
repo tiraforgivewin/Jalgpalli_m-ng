a28186ca14ab6831e30069417c6aa26d0b8c9b1b
opilane <opilane@HP-CZC2349HTN>
2024-09-25 11:18:54 +0300

    Etapp 2: mäng, pall ja staadion

---
+++ Jalgpalli_mäng/Ball.cs
@@ -0,0 +1,41 @@
+﻿namespace Football;
+
+public class Ball
+{
+    public double X { get; private set; }
+    public double Y { get; private set; }
+
+    private double _vx, _vy;
+
+    private Game _game;
+
+    public Ball(double x, double y, Game game)
+    {
+        _game = game;
+        X = x;
+        Y = y;
+    }
+
+    public void SetSpeed(double vx, double vy)
+    {
+        _vx = vx;
+        _vy = vy;
+    }
+
+    public void Move()
+    {
+        double newX = X + _vx;
+        double newY = Y + _vy;
+        if (_game.Stadium.IsIn(newX, newY))
+        {
+            X = newX;
+            Y = newY;
+        }
+        else
+        {
+            _vx = 0;
+            _vy = 0;
+        }
+    }
+
+}
---
+++ Jalgpalli_mäng/Stadium.cs
@@ -0,0 +1,19 @@
+﻿namespace Football;
+
+public class Stadium
+{
+    public Stadium(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public int Width { get; }
+
+    public int Height { get; }
+
+    public bool IsIn(double x, double y)
+    {
+        return x >= 0 && x < Width && y >= 0 && y < Height;
+    }
+}
